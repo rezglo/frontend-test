136a0ba5fdb874b8e319d77a57537dee2bc6bbc7
Angel Dario <dariogomez1011@gmail.com>
2023-08-22 11:38:11 -0400

    add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Slack clone</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>}>
+          </Route>
+        </Routes>
+    </Router>
     </div>
   );
 }
