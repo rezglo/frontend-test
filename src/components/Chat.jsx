922a7827c6b5193d02199adcd616ea2a2c1af129
Angel Dario <dariogomez1011@gmail.com>
2023-08-23 16:41:46 -0400

    create chat header section style

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,14 +1,68 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
 function Chat() {
   return (
     <ChatContainer>
-      <h1>Chat component</h1>
+      <Header>
+        <HeaderLeft>
+          <h4>
+            <strong>#channel-name</strong>
+          </h4>
+          <StarBorderIcon />
+        </HeaderLeft>
+        <HeaderRight>
+          <p>
+            <InfoOutlinedIcon /> Details
+          </p>
+        </HeaderRight>
+      </Header>
     </ChatContainer>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
 
-const ChatContainer = styled.div``;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  border-bottom: 1px solid lightgray;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+  
+  > h4 {
+    display: flex;
+    text-transform: lowercase;
+    margin-right: 10px;
+  }
+
+  > h4 > .MuiSvgIcon-root {
+    margin-left: 10px;
+    font-size: 18px;
+  }
+`;
+
+const HeaderRight = styled.div`
+  > p {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  }
+
+  > p > .MuiSvgIcon-root {
+    margin-right: 5px !important;
+    font-size: 16px;
+  }
+`;
+
+const ChatContainer = styled.div`
+  flex: 0.7;
+  flex-grow: 1;
+  overflow-y: scroll;
+`;
