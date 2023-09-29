3bf9b2678de121c414d0976dcc62b6fabd54b62d
SamyCode <sambea2103@gmail.com>
2023-09-29 10:05:45 -0400

    mock: add data.json fake data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
+import Page404 from './features/404/Page404'
+import HomePage from './features/Home/HomePage'
 
 function App () {
   return (
     <>
       <Routes>
-        <Route path='/' element={<h1>Slack CLone</h1>} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='*' element={<Page404 />} />
       </Routes>
     </>
   )
---
+++ src/features/404/Page404.tsx
@@ -0,0 +1,7 @@
+function Page404 () {
+  return (
+    <div>404</div>
+  )
+}
+
+export default Page404
