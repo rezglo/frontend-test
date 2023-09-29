db7a2991e312ec609c3c00c515aa0425c47ec928
SamyCode <sambea2103@gmail.com>
2023-09-29 11:43:24 -0400

    router: Create ProtectedRoute component to prevent anonymous users

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import Page404 from './features/404/Page404'
 import HomePage from './features/Home/HomePage'
 import Login from './features/Login/Login'
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
 
 function App () {
   return (
     <>
       <Routes>
-        <Route path='/a' element={<HomePage />} />
-        <Route path='/' element={<Login />} />
+        <Route path='/' element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+        <Route path='/sign-in' element={<Login />} />
         <Route path='*' element={<Page404 />} />
       </Routes>
     </>
--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,20 +1,21 @@
 import { Button, Input, Typography } from '@mui/material'
 import logo from '../../assets/slack.svg'
 import { useLocation, useNavigate } from 'react-router-dom'
-import { type ChangeEvent, type FormEvent, useState } from 'react'
+import { type ChangeEvent, type FormEvent, useState, useEffect } from 'react'
 import { useUserStore } from '../../store/users'
 import { type InputUser } from '../../store/types'
 import './Login.css'
 
 function Login () {
   const [form, setForm] = useState<InputUser>({} as InputUser)
-  const users = useUserStore(state => state.users)
   const getUsers = useUserStore(state => state.getUsers)
   const signInUser = useUserStore(state => state.signInUser)
   const navigate = useNavigate()
   const location = useLocation()
 
-  getUsers()
+  useEffect(() => {
+    getUsers()
+  }, [])
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setForm({
@@ -35,23 +36,17 @@ function Login () {
   }
   return (
     <section className='login'>
-      {
-        users.length === 0
-          ? <p>Loading...</p>
-          : (
-            <div className='login-inner'>
-              <img src={logo} alt="" />
-              <Typography variant='subtitle1'>Sign in to Slack</Typography>
-              <form action="" className='login-form' onSubmit={handleSubmit}>
-                <Input name='email' type='email' onChange={handleChange} placeholder='Email' />
-                <Input name='password' type='password' onChange={handleChange} placeholder='Password' />
-                <Button variant='contained' color='info' type='submit'>
-                  Sign in
-                </Button>
-              </form>
-            </div>
-            )
-      }
+      <div className='login-inner'>
+        <img src={logo} alt="" />
+        <Typography variant='subtitle1'>Sign in to Slack</Typography>
+        <form action="" className='login-form' onSubmit={handleSubmit}>
+          <Input name='email' type='email' onChange={handleChange} placeholder='Email' />
+          <Input name='password' type='password' onChange={handleChange} placeholder='Password' />
+          <Button variant='contained' color='info' type='submit'>
+            Sign in
+          </Button>
+        </form>
+      </div>
     </section>
   )
 }
