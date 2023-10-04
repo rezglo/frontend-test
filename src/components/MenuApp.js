57fbedf6cf35a4eb564335963603c86e73a604a6
addielGit <avelazcopadilla@gmail.com>
2023-10-05 01:15:31 +0200

    Adding component to the menu.

---
+++ src/components/MenuApp.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  ClusterOutlined,
+  UsergroupAddOutlined
+} from '@ant-design/icons';
+
+import Icon from '@ant-design/icons';
+
+import { Menu } from 'antd';
+import { Link } from 'react-router-dom';
+
+const MenuApp = () => {
+  return (
+    <Menu
+      theme="dark"
+      mode="inline"
+      defaultSelectedKeys={['1']}
+    >
+      <Menu.Item key="1">
+        <Icon component={ClusterOutlined} />
+        <span>
+          <Link to={'/channel'}>Channel</Link>
+        </span>
+      </Menu.Item>   
+
+      <Menu.Item key="2">
+        <Icon component={UsergroupAddOutlined} />
+        <span>
+          <Link to={'/users'}>Users</Link>
+        </span>
+      </Menu.Item>     
+    </Menu>
+  );
+};
+
+export default MenuApp;
+
+
+
+
+
+
