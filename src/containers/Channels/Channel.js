ec7e148f7ad52cdf18e498d0e87563aab39b8809
addielGit <avelazcopadilla@gmail.com>
2023-10-05 00:49:02 +0200

    Adding container for channels.

---
+++ src/containers/Channels/Channel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Channels from '../../components/Channels/Channels'
+
+const Channel = () => {
+  return (
+    <Channels />       
+  );
+};
+
+export default Channel;
