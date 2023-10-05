d218fd40319b999f6b9c6aba82193efd4b18d9b6
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-10-05 17:21:47 -0400

    feat(home): Create home component

---
+++ src/features/home/Home.tsx
@@ -0,0 +1,26 @@
+import { Button, Typography } from '@mui/material'
+
+export const Home = () => {
+  return (
+    <div className="grid place-items-center w-full h-full">
+      <div className="px-10 text-center w-full h-full">
+        <img className="w-40 h-40 mb-24" src="/slack-logo.svg" alt="Slack Logo" />
+        <Typography variant="h4" component="h1" className="font-extrabold mb-8">
+          Welcome to Slack
+        </Typography>
+        <Typography variant="h6" component="h2" className=" font-lighttext-base text-gray-500">
+          Slack brings all your team communication into one place, makes it all instantly searchable
+          and available wherever you go.
+        </Typography>
+
+        <Typography variant="h6" component="h2" className="text-sm text-gray-400">
+          Our aim is to make your working life simpler, more pleasant and more productive.
+        </Typography>
+
+        <Button className="mt-[30px] bg-green-600 text-white rounded-sm p-3" onClick={() => {}}>
+          Create Channel
+        </Button>
+      </div>
+    </div>
+  )
+}
