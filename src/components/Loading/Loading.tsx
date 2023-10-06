64212ac1f287632be6d421e37bc2a07414c7ca04
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-10-05 22:05:11 -0400

    refactor(): Delete loading text

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,11 +1,8 @@
-import { CircularProgress, Typography } from '@mui/material'
+import { CircularProgress } from '@mui/material'
 
 export const Loading = () => {
   return (
     <div className="h-full flex w-full flex-col flex-auto items-center justify-center flex-shrink-0 p-16 md:p-24">
-      <Typography component="h1" className="text-13 sm:text-20 mb-16" color="textSecondary">
-        Loading...
-      </Typography>
       <CircularProgress className="text-slack" />
     </div>
   )
