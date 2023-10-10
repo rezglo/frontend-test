f7190c2337c34102a0d974229452d3482899ea80
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-10-10 13:47:32 -0400

    feat(utils): Create format date utilities

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,3 +1,1 @@
 export const formatToTime = (timestamp: string) => new Date(timestamp).toLocaleTimeString('en-US')
-
-export const formatToDate = (timestamp: string) => new Date(timestamp).toLocaleTimeString('en-US')
