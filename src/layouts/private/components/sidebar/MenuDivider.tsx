d5bbce5516ddded2abf3d66159322249c68b5326
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-10-10 13:32:42 -0400

    chore(): Move to inside components folder

--- src/layouts/private/components/sidebar/MenuDivider.tsx
+++
@@ -1,5 +0,0 @@
-import { Divider } from '@mui/material'
-
-export const MenuDivider = () => {
-  return <Divider className="bg-slackLight h-[2px]" />
-}
--- src/layouts/root/Root.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom'
-
-export const RootLayout = () => {
-  return (
-    <div className="flex h-screen flex-col items-center justify-start bg-[#f8f8f8]">
-      <Outlet />
-    </div>
-  )
-}
