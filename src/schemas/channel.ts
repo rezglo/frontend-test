7de9ad9744faad042212271525c467282baf369c
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-10-16 10:03:52 -0400

    refactor(): Add enum to status property in the schema

--- src/schemas/channel.ts
+++ src/schemas/channel.ts
@@ -1,8 +1,9 @@
+import { ChannelType } from '@/features/dashboard/channel/types'
 import { z } from 'zod'
 
 export const addChannelSchema = z.object({
   name: z.string().min(1, 'You must provide a name'),
-  status: z.string(),
+  type: z.nativeEnum(ChannelType),
 })
 
 export type AddChannelSchema = z.infer<typeof addChannelSchema>
