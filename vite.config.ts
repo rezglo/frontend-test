84a3844de0a05347671a8f045b6e2d0399f9d32b
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-10-16 10:01:16 -0400

    test(): Config for tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -6,5 +7,9 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
   },
 })
