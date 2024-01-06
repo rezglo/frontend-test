84477465abc8ff2ee56abb9294f23fee2b069fd5
Pedro Hourrutiner <pedrodavidhourrutinerdiaz@gmail.com>
2024-01-06 13:55:51 -0500

    Daisyui configured

---
+++ src/SlackApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { LoginScreen } from './views/LoginScreen'
+
+export const SlackApp = () => {
+  return (
+    <div>
+        <LoginScreen />
+    </div>
+  )
+}
---
+++ src/views/LoginScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const LoginScreen = () => {
+  return (
+    <div className='h-full w-full flex justify-center items-center' >
+        <div>
+            
+        </div>
+    </div>
+  )
+}
