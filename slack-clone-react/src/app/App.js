0cfb2e7e5efb7e2a1e300430b74a75406a6a9c18
randy930529 <randy930529@gmail.com>
2024-01-07 12:43:10 -0500

    set App to add redux provider and app router

--- slack-clone-react/src/app/App.js
+++ slack-clone-react/src/app/App.js
@@ -1,7 +1,19 @@
 import "../styles/App.css";
+import { Provider } from "react-redux";
+import store from "../redux/store";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RootLayout from "../ui/hocs/Layout";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Slack clone</h1>;
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<RootLayout />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
 };
 
 export default App;
