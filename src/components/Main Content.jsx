43626a1e8b2bcd597aff3b70dd4408f3ec037fd1
Pedro Hourrutiner <pedrodavidhourrutinerdiaz@gmail.com>
2024-01-07 12:38:48 -0500

    NothingSelected implemented

---
+++ src/components/Main Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NothingSelected } from './NothingSelected'
+
+export const MainContent = () => {
+  return (
+    <div className='w-full h-vh'>
+        <NothingSelected />
+    </div>
+  )
+}
--- src/views/HomeScreen.jsx
+++ src/views/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { SideBar } from '../components/sidebar/SideBar'
+import { MainContent } from '../components/Main Content'
 
 export const HomeScreen = () => {
   return (
     <div className='flex ' >
         
             <SideBar />
+            <MainContent />
         
     </div>
   )
