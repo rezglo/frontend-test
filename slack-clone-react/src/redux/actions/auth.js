cb4ea31bad0dfc4d4a64c115cdd04adb66d821a5
randy930529 <randy930529@gmail.com>
2024-01-09 11:26:47 -0500

    add uuid4 for authentication accsess

--- slack-clone-react/src/redux/actions/auth.js
+++ slack-clone-react/src/redux/actions/auth.js
@@ -1,17 +1,189 @@
+import { signAccess } from "../../lib/UUID";
+import { setAlert } from "./alert";
 import {
   SIGNUP_SUCCESS,
   SIGNUP_FAIL,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  AUTHENTICATED_SUCCESS,
+  AUTHENTICATED_FAIL,
+  USER_LOADED_SUCCESS,
+  USER_LOADED_FAIL,
 } from "./types";
 
-export const check_authenticated = () => async (dispatch) => {};
+export const check_authenticated = () => async (dispatch) => {
+  try {
+    const access = localStorage.getItem("access");
+
+    if (access) {
+      dispatch({
+        type: AUTHENTICATED_SUCCESS,
+      });
+    } else {
+      dispatch({
+        type: AUTHENTICATED_FAIL,
+      });
+    }
+  } catch (err) {
+    dispatch({
+      type: AUTHENTICATED_FAIL,
+    });
+  }
+};
 
 export const signup =
-  (first_name, last_name, email, password, re_password) =>
-  async (dispatch) => {};
+  (first_name, last_name, email, password, re_password) => async (dispatch) => {
+    try {
+      if (password !== re_password) {
+        dispatch({
+          type: SIGNUP_FAIL,
+        });
+        dispatch(
+          setAlert("Repeat password does not match the password.", "red")
+        );
+      }
 
-export const login = (email, password) => async (dispatch) => {};
+      const user = {
+        id: -1,
+        first_name,
+        last_name,
+        email,
+        password,
+      };
 
-export const logout = () => (dispatch) => {};
+      const auths = localStorage.getItem("auths");
+      let authsArray = [];
+
+      if (
+        auths !== null &&
+        auths !== undefined &&
+        auths !== "null" &&
+        auths !== "undefined"
+      )
+        authsArray = JSON.parse(auths);
+
+      user.id = authsArray.length;
+      authsArray.push(user);
+
+      const res = {
+        auths: JSON.stringify(authsArray),
+      };
+
+      if (res) {
+        dispatch({
+          type: SIGNUP_SUCCESS,
+          payload: res,
+        });
+        dispatch(setAlert("Account was successfully registered.", "green"));
+      } else {
+        dispatch({
+          type: SIGNUP_FAIL,
+        });
+        dispatch(setAlert("Error creating account.", "red"));
+      }
+    } catch (err) {
+      console.log(err);
+      dispatch({
+        type: SIGNUP_FAIL,
+      });
+      dispatch(
+        setAlert("Error connecting to the server, try again later.", "red")
+      );
+    }
+  };
+
+export const load_user = () => async (dispatch) => {
+  const access = localStorage.getItem("access");
+  const auths = localStorage.getItem("auths");
+
+  if (
+    access &&
+    auths &&
+    auths !== null &&
+    auths !== undefined &&
+    auths !== "null" &&
+    auths !== "undefined"
+  ) {
+    try {
+      const authsArray = JSON.parse(auths);
+      const userId = access.split("-")[0];
+      const user = authsArray.find((object) => `${object.id}` === userId);
+      const res = { data: user };
+
+      if (res) {
+        dispatch({
+          type: USER_LOADED_SUCCESS,
+          payload: res.data,
+        });
+      } else {
+        dispatch({
+          type: USER_LOADED_FAIL,
+        });
+      }
+    } catch (err) {
+      dispatch({
+        type: USER_LOADED_FAIL,
+      });
+    }
+  } else {
+    dispatch({
+      type: USER_LOADED_FAIL,
+    });
+  }
+};
+
+export const login = (email, password) => async (dispatch) => {
+  const auths = localStorage.getItem("auths");
+
+  try {
+    let authsArray = [];
+
+    if (
+      auths !== null &&
+      auths !== undefined &&
+      auths !== "null" &&
+      auths !== "undefined"
+    )
+      authsArray = JSON.parse(auths);
+
+    const user = authsArray.find((object) => object.email === email);
+    if (user.password !== password) {
+      dispatch({
+        type: LOGIN_FAIL,
+      });
+    }
+
+    const access = `${user.id}-${signAccess()}`;
+
+    const res = {
+      access,
+    };
+
+    if (res) {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res,
+      });
+      dispatch(load_user());
+      dispatch(setAlert("Successful login.", "green"));
+    } else {
+      dispatch({
+        type: LOGIN_FAIL,
+      });
+      dispatch(setAlert("Failed to login.", "red"));
+    }
+  } catch (err) {
+    dispatch({
+      type: LOGIN_FAIL,
+    });
+    dispatch(setAlert("Failed to login, try again later.", "red"));
+  }
+};
+
+export const logout = () => (dispatch) => {
+  dispatch({
+    type: LOGOUT,
+  });
+  dispatch(setAlert("Succesfully logged out", "green"));
+};
--- slack-clone-react/src/redux/reducers/auth.js
+++ slack-clone-react/src/redux/reducers/auth.js
@@ -4,24 +4,73 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  AUTHENTICATED_SUCCESS,
+  AUTHENTICATED_FAIL,
+  USER_LOADED_SUCCESS,
+  USER_LOADED_FAIL,
 } from "../actions/types";
 
 const initialState = {
+  auths: localStorage.getItem("auths"),
   access: localStorage.getItem("access"),
   isAuthenticated: null,
   user: null,
-  loading: false,
 };
 
 export default function Auth(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
-    case "":
+    case USER_LOADED_SUCCESS:
       return {
         ...state,
+        user: payload,
+      };
+    case USER_LOADED_FAIL:
+      return {
+        ...state,
+        user: null,
+      };
+    case AUTHENTICATED_SUCCESS:
+      return {
+        ...state,
+        isAuthenticated: true,
+      };
+    case AUTHENTICATED_FAIL:
+      localStorage.removeItem("access");
+      return {
+        ...state,
+        isAuthenticated: false,
+        access: null,
+      };
+    case LOGIN_SUCCESS:
+      localStorage.setItem("access", payload.access);
+      return {
+        ...state,
+        isAuthenticated: true,
+        access: localStorage.getItem("access"),
+      };
+    case SIGNUP_SUCCESS:
+      localStorage.removeItem("access");
+      localStorage.setItem("auths", payload.auths);
+      return {
+        ...state,
+        auths: localStorage.getItem("auths"),
+        access: null,
+        isAuthenticated: false,
+        user: null,
       };
 
+    case SIGNUP_FAIL:
+    case LOGIN_FAIL:
+    case LOGOUT:
+      localStorage.removeItem("access");
+      return {
+        ...state,
+        access: null,
+        isAuthenticated: false,
+        user: null,
+      };
     default:
       return state;
   }
