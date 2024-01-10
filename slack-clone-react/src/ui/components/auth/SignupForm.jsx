758c42b5f64c82491c895c061cb35be826ba08ed
randy930529 <randy930529@gmail.com>
2024-01-10 01:52:03 -0500

    add Toaster notifications

--- slack-clone-react/src/ui/components/auth/SignupForm.jsx
+++ slack-clone-react/src/ui/components/auth/SignupForm.jsx
@@ -5,6 +5,7 @@ import { Navigate } from "react-router";
 import { connect } from "react-redux";
 import { signup } from "../../../redux/actions/auth";
 import { useEffect, useState } from "react";
+import RootLayout from "../../hocs/RootLayout";
 
 const SignupForm = ({ signup }) => {
   useEffect(() => {
@@ -34,126 +35,128 @@ const SignupForm = ({ signup }) => {
   if (accountCreated) return <Navigate to="/" />;
 
   return (
-    <main className="flex min-h-screen flex-col p-40 pt-6">
-      <form className="space-y-3" onSubmit={(e) => onSubmit(e)}>
-        <div className="flex-1 rounded-lg bg-fuchsia-950 px-6 pb-4 pt-8">
-          <h1 className={`mb-3 text-2xl text-gray-50`}>
-            Please Sign up to continue.
-          </h1>
-          <div className="w-full">
-            <div>
-              <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-50"
-                htmlFor="name"
-              >
-                Name
-              </label>
-              <div className="relative">
-                <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                  id="first_name"
-                  type="text"
-                  name="first_name"
-                  value={first_name}
-                  onChange={(e) => onChange(e)}
-                  placeholder="Enter your name"
-                  required
-                />
+    <RootLayout>
+      <div className="flex min-h-screen flex-col p-40 pt-6">
+        <form className="space-y-3" onSubmit={(e) => onSubmit(e)}>
+          <div className="flex-1 rounded-lg bg-fuchsia-950 px-6 pb-4 pt-8">
+            <h1 className={`mb-3 text-2xl text-gray-50`}>
+              Please Sign up to continue.
+            </h1>
+            <div className="w-full">
+              <div>
+                <label
+                  className="mb-3 mt-5 block text-xs font-medium text-gray-50"
+                  htmlFor="name"
+                >
+                  Name
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    id="first_name"
+                    type="text"
+                    name="first_name"
+                    value={first_name}
+                    onChange={(e) => onChange(e)}
+                    placeholder="Enter your name"
+                    required
+                  />
+                </div>
               </div>
-            </div>
-            <div>
-              <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-50"
-                htmlFor="lastname"
-              >
-                Last Name
-              </label>
-              <div className="relative">
-                <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                  id="last_name"
-                  type="text"
-                  name="last_name"
-                  value={last_name}
-                  onChange={(e) => onChange(e)}
-                  placeholder="Enter your last name"
-                  required
-                />
+              <div>
+                <label
+                  className="mb-3 mt-5 block text-xs font-medium text-gray-50"
+                  htmlFor="lastname"
+                >
+                  Last Name
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    id="last_name"
+                    type="text"
+                    name="last_name"
+                    value={last_name}
+                    onChange={(e) => onChange(e)}
+                    placeholder="Enter your last name"
+                    required
+                  />
+                </div>
               </div>
-            </div>
-            <div>
-              <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-50"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <div className="relative">
-                <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                  id="email"
-                  type="email"
-                  name="email"
-                  value={email}
-                  onChange={(e) => onChange(e)}
-                  placeholder="Enter your email address"
-                  required
-                />
-                <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-              </div>
-            </div>
-            <div className="mt-4">
-              <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-50"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <div className="relative">
-                <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                  id="password"
-                  type="password"
-                  name="password"
-                  value={password}
-                  onChange={(e) => onChange(e)}
-                  placeholder="Enter password"
-                  required
-                  minLength={6}
-                />
-                <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <div>
+                <label
+                  className="mb-3 mt-5 block text-xs font-medium text-gray-50"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    id="email"
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => onChange(e)}
+                    placeholder="Enter your email address"
+                    required
+                  />
+                  <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                </div>
               </div>
               <div className="mt-4">
                 <label
                   className="mb-3 mt-5 block text-xs font-medium text-gray-50"
                   htmlFor="password"
                 >
-                  Re-Password
+                  Password
                 </label>
                 <div className="relative">
                   <input
                     className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                    id="re_password"
+                    id="password"
                     type="password"
-                    name="re_password"
-                    value={re_password}
+                    name="password"
+                    value={password}
                     onChange={(e) => onChange(e)}
-                    placeholder="Enter re-password"
+                    placeholder="Enter password"
                     required
                     minLength={6}
                   />
                   <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
                 </div>
+                <div className="mt-4">
+                  <label
+                    className="mb-3 mt-5 block text-xs font-medium text-gray-50"
+                    htmlFor="password"
+                  >
+                    Re-Password
+                  </label>
+                  <div className="relative">
+                    <input
+                      className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                      id="re_password"
+                      type="password"
+                      name="re_password"
+                      value={re_password}
+                      onChange={(e) => onChange(e)}
+                      placeholder="Enter re-password"
+                      required
+                      minLength={6}
+                    />
+                    <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                  </div>
+                </div>
               </div>
             </div>
+            <SignupFormButton />
+            <div className="flex h-8 items-end space-x-1">
+              {/* Add form errors here */}
+            </div>
           </div>
-          <SignupFormButton />
-          <div className="flex h-8 items-end space-x-1">
-            {/* Add form errors here */}
-          </div>
-        </div>
-      </form>
-    </main>
+        </form>
+      </div>
+    </RootLayout>
   );
 };
 
