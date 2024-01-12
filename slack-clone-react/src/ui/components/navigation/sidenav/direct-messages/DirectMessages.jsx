e88e69c8e340bb98fd49f2840315b759e09b5cfa
randy930529 <randy930529@gmail.com>
2024-01-11 21:59:20 -0500

    update DirectMessages

--- slack-clone-react/src/ui/components/navigation/sidenav/direct-messages/DirectMessages.jsx
+++ slack-clone-react/src/ui/components/navigation/sidenav/direct-messages/DirectMessages.jsx
@@ -1,33 +1,67 @@
+import { connect } from "react-redux";
 import { Stacked } from "../../../messages/Stacked";
 import NavLinks from "../../NavLinks";
 import NavLinksHead from "../../NavLinksHead";
 import { PlayIcon } from "@heroicons/react/24/outline";
-
-const urlImage =
-  "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80";
+import { useCallback, useEffect, useState } from "react";
+import { get_direct_messages } from "../../../../../redux/actions/directMessages";
 
 const linkHead = { name: "Direct Messages", icon: PlayIcon };
-const profile = () => <Stacked urlImage={urlImage} width="w-6 h-6" />;
+const profile = (urlImage) => <Stacked urlImage={urlImage} width="w-6 h-6" />;
 
-const links = [
-  { name: "Leslie", href: "/dashboard", icon: profile },
-  {
-    name: "Dairelis",
-    href: "/dashboard/invoices",
-    icon: profile,
-  },
-  { name: "Dariel", href: "/dashboard/customers", icon: profile },
-];
+let links = [];
 
-const DirectMessages = (props) => {
+const createLinks = (directMessages) => {
+  directMessages.forEach((directMessage, index) => {
+    links.push({
+      id: index,
+      name: "Dory",
+      href: "#",
+      icon: profile(directMessage.url),
+    });
+  });
+};
+
+const DirectMessages = ({ directMessages, get_direct_messages }) => {
+  const [directMessagesNow, setDirectMessagesNow] = useState([]);
+  const [render, setRender] = useState(true);
+
+  const memoryGetData = useCallback(async () => {
+    await get_direct_messages();
+  }, [get_direct_messages]);
+
+  useEffect(() => {
+    memoryGetData();
+    setTimeout(() => {
+      setRender(!render);
+    }, 5000);
+  }, [memoryGetData, render]);
+
+  useEffect(() => {
+    setDirectMessagesNow(directMessages);
+  }, [directMessages]);
+
+  if (directMessagesNow) {
+    links = [];
+    createLinks(directMessagesNow);
+  }
+
   return (
     <>
       <ul>
         <NavLinksHead link={linkHead} />
-        <NavLinks links={links} />
+        <NavLinks links={links}>
+          <span className="absolute w-3 h-3 left-4 rounded-full bg-green-900 top-4 text-gray-400 hover:text-gray-500 sm:left-6 sm:top-8 md:left-6 md:top-6 lg:left-8 lg:top-8"></span>
+        </NavLinks>
       </ul>
     </>
   );
 };
 
-export default DirectMessages;
+const mapStateToProps = (state) => ({
+  directMessages: state.DirectMessager.directMessages,
+});
+
+export default connect(mapStateToProps, {
+  get_direct_messages,
+})(DirectMessages);
