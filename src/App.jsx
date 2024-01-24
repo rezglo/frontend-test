7051deec06eaf4906f9b5356b03e6682a6fa820b
Adrian Marcos <adrimarcos1996@gmail.com>
2024-01-23 21:22:24 -0500

    Creating project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="w-full h-screen flex flex-col bg-gradient-to-br from-[#714c91] to-[#3a2d47] min-[981px]:pb-1 min-[981px]:pr-1">
+    </div>
+  )
+}
+
+export default App
